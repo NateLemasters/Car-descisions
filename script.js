a54b7957165d18c9fe7ac86281a1e1cb75bf5844
Joseph11623 <jadair28@students.stab.org>
2024-11-07 16:14:30 -0500

    w

--- script.js
+++ script.js
@@ -7,8 +7,5 @@ console.log(Age);
 console.log(Salary);
 console.log(Place);
 console.log(Children);
-var outputBox = document.getElementById("outputBox").value;
-var feedback;
-if (age < 16) {
-    feedback = "No car for you! You are too young!";
-}
+
+
