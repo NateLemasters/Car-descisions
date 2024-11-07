fc81a8581ef8f8177a8cdaeb2b6c157d530e9673
NateLemasters <nlemasters28@students.stab.org>
2024-11-07 16:16:47 -0500

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-var Age = document.getElementById("Age").value;
-var Salary = document.getElementById("Salary").value;
-var Place = document.getElementById("Place").value;
-var Children = document.getElementById("Children").value;
-console.log(Age);
-console.log(Salary);
-console.log(Place);
-console.log(Children);
+
+
+
+function getCar(){
+    var Age = document.getElementById("Age").value;
+    var Salary = document.getElementById("Salary").value;
+    var Place = document.getElementById("Place").value;
+    var Children = document.getElementById("Children").value;
+
+    console.log(Age);
+    console.log(Salary);
+    console.log(Place);
+    console.log(Children);
+
+    if(Age == "Over 16" && Children == "You have Children" && Place == "City" && Salary == "Over 100k"){
+        console.log("Hi");
+    } 
+    if(Age == "Over 16" && Children == "You have Children" && Place == "Countryside" && Salary == "Over 100k"){
+        console.log("hi");
+    }
+    if(Age == "Over 16" && Children == "You have Children" && Place == "Countryside" && Salary == "under 100k"){
+        console.log("hi");
+    }
+    if(Age == "Over 16" && Children == "You have Children" && Place == "City" && Salary == "under 100k"){
+        console.log("hi");
+    }
+    
+    
+    if(Age == "Over 16" && Children == "No Children" && Place == "City" && Salary == "Over 100k"){
+        console.log("Hi");
+    }
+    if(Age == "Over 16" && Children == "No Children" && Place == "Countryside" && Salary == "Over 100k"){
+        console.log("Hi");
+    }
+    if(Age == "Over 16" && Children == "No Children" && Place == "Countryside" && Salary == "under 100k"){
+        console.log("hi");
+    }
+    if(Age == "Over 16" && Children == "No Children" && Place == "City" && Salary == "under 100k"){
+        console.log("hi");
+    }
+    
+    
+}
