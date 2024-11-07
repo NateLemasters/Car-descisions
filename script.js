6c14da1601f8ded017e942cfa9e6dab4c1ce5f68
Joseph11623 <jadair28@students.stab.org>
2024-11-07 10:09:38 -0500

    v

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ var Age = document.getElementById("Age").value;
 var Salary = document.getElementById("Salary").value;
 var Place = document.getElementById("Place").value;
 var Children = document.getElementById("Children").value;
+
 console.log(Age);
 console.log(Salary);
 console.log(Place);
 console.log(Children);
+var outputBox = document.getElementById("outputBox").value;
+var feedback;
+if (age < 16) {
+    feedback = "No car for you! You are too young!";
+}
