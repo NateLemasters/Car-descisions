396bba8cdbc11c03a8f6a4bf45f3bd7b2f5bec3a
Joseph11623 <jadair28@students.stab.org>
2024-11-07 09:52:36 -0500

    j

---
+++ script.js
@@ -0,0 +1,8 @@
+var Age = document.getElementById("Age").value;
+var Salary = document.getElementById("Salary").value;
+var Place = document.getElementById("Place").value;
+var Children = document.getElementById("Children").value;
+console.log(Age);
+console.log(Salary);
+console.log(Place);
+console.log(Children);
